65ed4559e33607dc4839f466b384df50f9323945
wtks <30363887+wtks@users.noreply.github.com>
2022-07-23 16:46:35 +0900

    open conn制限

--- app/webapp/go/isuports.go
+++ app/webapp/go/isuports.go
@@ -143,8 +143,8 @@ func getConnection(id int64, fillDBN bool) (*sqlx.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	db.SetMaxOpenConns(10)
-	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetMaxIdleConns(100)
 
 	return db, nil
 }
