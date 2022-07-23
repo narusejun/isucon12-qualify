43ce30cce46fde30c40f39f23b32351a2b046952
Kazuki Sawada <kazuki@6715.jp>
2022-07-23 15:57:20 +0900

    vz

--- app/webapp/go/isuports.go
+++ app/webapp/go/isuports.go
@@ -100,7 +100,7 @@ var (
 
 func getConnection(id int64, fillDBN bool) (*sqlx.DB, error) {
 	host := "isuports-2.t.isucon.dev"
-	if id == 1 {
+	if id < 200 && id/10%2 == 0 {
 		host = "isuports-3.t.isucon.dev"
 	}
 
