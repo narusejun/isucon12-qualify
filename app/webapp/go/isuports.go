a8310e8f13eb25f3814a4db3a40649706fe5f5ea
wtks <30363887+wtks@users.noreply.github.com>
2022-07-23 13:06:05 +0900

    N破壊

--- app/webapp/go/isuports.go
+++ app/webapp/go/isuports.go
@@ -1252,15 +1252,6 @@ func playerHandler(c echo.Context) error {
 		}
 		return fmt.Errorf("error retrievePlayer: %w", err)
 	}
-	cs := []CompetitionRow{}
-	if err := tenantDB.SelectContext(
-		ctx,
-		&cs,
-		"SELECT * FROM competition WHERE tenant_id = ? ORDER BY created_at ASC",
-		v.tenantID,
-	); err != nil && !errors.Is(err, sql.ErrNoRows) {
-		return fmt.Errorf("error Select competition: %w", err)
-	}
 
 	// player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する
 	fl, err := flockByTenantID(v.tenantID)
@@ -1268,32 +1259,26 @@ func playerHandler(c echo.Context) error {
 		return fmt.Errorf("error flockByTenantID: %w", err)
 	}
 	defer fl.Close()
-	pss := make([]PlayerScoreRow, 0, len(cs))
-	for _, c := range cs {
-		ps := PlayerScoreRow{}
-		if err := tenantDB.GetContext(
-			ctx,
-			&ps,
-			// 最後にCSVに登場したスコアを採用する = row_numが一番大きいもの
-			"SELECT score FROM player_score WHERE tenant_id = ? AND competition_id = ? AND player_id = ? ORDER BY row_num DESC LIMIT 1",
-			v.tenantID,
-			c.ID,
-			p.ID,
-		); err != nil {
-			// 行がない = スコアが記録されてない
-			if errors.Is(err, sql.ErrNoRows) {
-				continue
-			}
-			return fmt.Errorf("error Select player_score: tenantID=%d, competitionID=%s, playerID=%s, %w", v.tenantID, c.ID, p.ID, err)
-		}
-		ps.CompetitionTitle = c.Title
-		pss = append(pss, ps)
+
+	type Row struct {
+		Score int64  `db:"score"`
+		Title string `db:"title"`
+	}
+	pss := []Row{}
+	if err := tenantDB.GetContext(
+		ctx,
+		&pss,
+		"SELECT competition.title, score FROM player_score INNER JOIN competition ON player_score.competition_id = competition.id WHERE player_id = ? AND row_num IN (SELECT MAX(row_num) FROM player_score WHERE player_id = ? GROUP BY competition_id) ORDER BY competition.created_at ASC",
+		p.ID,
+		p.ID,
+	); err != nil {
+		return fmt.Errorf("error Select player_score: tenantID=%d, playerID=%s, %w", v.tenantID, p.ID, err)
 	}
 
 	psds := make([]PlayerScoreDetail, 0, len(pss))
 	for _, ps := range pss {
 		psds = append(psds, PlayerScoreDetail{
-			CompetitionTitle: ps.CompetitionTitle,
+			CompetitionTitle: ps.Title,
 			Score:            ps.Score,
 		})
 	}
