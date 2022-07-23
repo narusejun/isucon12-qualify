d98fe1575dd419365870d1028ecc49f7c3c531ce
Kazuki Sawada <kazuki@6715.jp>
2022-07-23 13:46:54 +0900

    grt

--- app/webapp/sql/init.sh
+++ app/webapp/sql/init.sh
@@ -14,7 +14,7 @@ mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
 		--host "$ISUCON_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < init.sql
+		"$ISUCON_DB_NAME" < init.sql &
 
 export id=1
 sh init_tenant_huge.sh &
