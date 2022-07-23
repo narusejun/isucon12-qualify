833cd77f6526605b26c7176c801ce0531bafd67f
Kazuki Sawada <kazuki@6715.jp>
2022-07-23 17:25:52 +0900

    stop unused middlewares

--- s1/deploy.sh
+++ s1/deploy.sh
@@ -13,17 +13,5 @@ make
 
 # ミドルウェア・Appの再起動
 sudo systemctl restart mysql
-sudo systemctl reload nginx
+sudo systemctl restart nginx
 sudo systemctl restart isuports
-
-# slow query logの有効化
-QUERY="
-set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
-set global long_query_time = 0;
-set global slow_query_log = ON;
-"
-echo $QUERY | mysql -uroot -proot
-
-# log permission
-sudo chmod 777 /var/log/nginx /var/log/nginx/*
-sudo chmod 777 /var/log/mysql /var/log/mysql/*
