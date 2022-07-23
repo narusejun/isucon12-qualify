8811c82c805d30a1ac7f0e0a052979c8cdba66aa
Kazuki Sawada <kazuki@6715.jp>
2022-07-23 10:37:12 +0900

    eliminate docker

--- s1/deploy.sh
+++ s1/deploy.sh
@@ -8,13 +8,13 @@ sudo cp -f etc/nginx/sites-available/isuports.conf /etc/nginx/sites-available/is
 sudo nginx -t
 
 # アプリケーションのビルド
-#cd /home/isucon/webapp/go
-#go build -o isucondition
+cd /home/isucon/webapp/go
+make
 
 # ミドルウェア・Appの再起動
-#sudo systemctl restart mariadb
-#sudo systemctl reload nginx
-#sudo systemctl restart isucondition.go
+sudo systemctl restart mariadb
+sudo systemctl reload nginx
+sudo systemctl restart isuports
 
 # slow query logの有効化
 QUERY="
--- s3/deploy.sh
+++ s3/deploy.sh
@@ -8,13 +8,13 @@ sudo cp -f env.sh /home/isucon/env.sh
 #sudo nginx -t
 
 # アプリケーションのビルド
-#cd /home/isucon/webapp/go
-#go build -o isucondition
+cd /home/isucon/webapp/go
+make
 
 # ミドルウェア・Appの再起動
-#sudo systemctl restart mariadb
-#sudo systemctl reload nginx
-#sudo systemctl restart isucondition.go
+sudo systemctl restart mariadb
+sudo systemctl reload nginx
+sudo systemctl restart isuports
 
 # slow query logの有効化
 QUERY="
