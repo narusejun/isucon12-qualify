92353907f1772eb19f09ca668745d106b8f317cd
Kazuki Sawada <kazuki@6715.jp>
2022-07-23 10:45:39 +0900

    fix

--- s1/deploy.sh
+++ s1/deploy.sh
@@ -22,7 +22,7 @@ set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
 set global long_query_time = 0;
 set global slow_query_log = ON;
 "
-echo $QUERY | sudo mysql -uroot
+echo $QUERY | mysql -uroot -proot
 
 # log permission
 sudo chmod 777 /var/log/nginx /var/log/nginx/*
